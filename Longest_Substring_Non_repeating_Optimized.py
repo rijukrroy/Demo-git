e3b253800e95c596f70b8bf21ae75e2d1bda55c9
Riju Kumar Roy <rijukrroy@gmail.com>
2024-10-08 18:58:36 +0530

    First commit

---
+++ Longest_Substring_Non_repeating_Optimized.py
@@ -0,0 +1,30 @@
+# Length of the longest substring with non-repeating characters
+# Example string:
+# - “ABCDEFGABEF” -> 7
+# - “GEEKSFORGEEKS” -> 7
+
+my_str = "GEEKSFORGEEKS"
+
+def isNonRepeated(astr):
+    alst = []
+    for elem in astr:
+        if elem not in alst:
+            alst.append(elem)
+
+    if astr == "".join(alst):
+        return True
+    else:
+        return False
+
+max_length = ""
+for i in range(len(my_str)):
+    for j in range(i+1,len(my_str)+1):
+
+        if(isNonRepeated(my_str[i:j])):
+            # print(my_str[i:j])
+            if len(my_str[i:j])> len(max_length):
+                max_length = my_str[i:j]
+        else:
+            break
+print("_______________________")
+print(max_length)
